2e8b688460370d128452559377884ce69bcd9b38
Jo Gayed <joseph.gayed91@gmail.com>
2024-03-20 09:42:59 +0200

    Use sanctum to Protect Single Api : Search , so that user can't use it without passing  Authorization : Bearer TOKEN_VALUE in the request headers

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,8 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+//make the search api protected by token
+Route::middleware('auth:sanctum')->get('/products/search', [ProductController::class, 'search']);
 
-Route::get('/products/search',[ProductController::class,'search']);
 //this will create routes apis for all crud operations
 Route::Resource('products',ProductController::class);
