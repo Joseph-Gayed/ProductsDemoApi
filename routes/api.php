2d491faa66cd47f485fed44963aafd4db3c2996a
Jo Gayed <joseph.gayed91@gmail.com>
2024-03-20 10:15:22 +0200

    Protect group of apis (create, update, delete) and make some apis public (list , select by id , search)

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,16 @@ use App\Http\Controllers\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-//make the search api protected by token
-Route::middleware('auth:sanctum')->get('/products/search', [ProductController::class, 'search']);
+//public routes
+Route::get('/products/search', [ProductController::class, 'search']);
+Route::get('/products',[ProductController::class,'index']);
+Route::get('/products/{id}',[ProductController::class,'show']);
 
-//this will create routes apis for all crud operations
-Route::Resource('products',ProductController::class);
+//Protected routes
+Route::group(['middleware' => ['auth:sanctum']],function(){
+    //here add the apis that should use token 
+    Route::post('/products',[ProductController::class,'store']);
+    Route::put('/products/{id}',[ProductController::class,'update']);
+    Route::delete('/products/{id}',[ProductController::class,'destroy']);
+});
+
